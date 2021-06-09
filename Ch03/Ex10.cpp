a2120efe865477caf3d43554c689aebcce28c130
Mohammad Hawa <66217824+mohammdhawa@users.noreply.github.com>
2021-06-09 15:56:00 +0300

    Create Ex10.cpp

---
+++ Ch03/Ex10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float amount, prinicap_amount, rat_interest, temp;
+	int year;
+	cout <<"Enter the final amount you want to get in particular year: ";
+	cin >>amount;
+	cout <<"Enter the principal amount: ";
+	cin >>prinicap_amount;
+	cout <<"Enter the rate of interest: ";
+	cin >>rat_interest;
+
+	temp = prinicap_amount;
+
+	while(temp<amount)
+	{
+		temp *= (1+(rat_interest/100));
+		year++;
+	}
+	cout <<"It will take " <<year <<" years at " <<rat_interest <<" of interest to reach the final amount " <<amount <<endl;
+	return 0;
+}
